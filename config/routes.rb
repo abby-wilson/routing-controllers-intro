6bcfc504dba33196519fc17fbc0c896925270cd6
Abby Wilson <abigail.wilson1@outlook.com>
2017-07-19 15:42:06 -0400

    all routes and controllers are now working yay

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
+root 'pages#welcome'
 get '/welcome' => 'pages#welcome'
-get '/' => 'pages#welcome'
 get '/about' => 'pages#about'
 get '/contest' => 'pages#contest'
 get '/kitten/:size'=> 'pages#kitten'

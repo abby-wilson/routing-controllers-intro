d544e2687c27e1db2a796cd6b8b8ba9682c09095
Abby Wilson <abigail.wilson1@outlook.com>
2017-07-19 15:01:37 -0400

    Made the kitten and kittens routes and controllers DRY using Before filters

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-
+ before_action :set_kitten_url, only: [:kitten, :kittens]
 def welcome
   @header = "Welcome!"
   render :welcome
@@ -14,6 +14,12 @@ def contest
 end
 
 def kitten
+end
+
+def kittens 
+end
+
+def set_kitten_url
   requested_size = params[:size]
   @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
 end

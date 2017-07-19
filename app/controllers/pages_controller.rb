3278b8691815220fcd19603286f4b53d1f9d5cae
Abby Wilson <abigail.wilson1@outlook.com>
2017-07-19 15:21:07 -0400

    added magic work route and controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,18 +10,29 @@ def about
 end
 
 def contest
-  render :contest
+  flash[:notice] = "Sorry, the contest has ended"
+  redirect_to "/welcome"
 end
 
 def kitten
 end
 
-def kittens 
+def kittens
 end
 
 def set_kitten_url
   requested_size = params[:size]
   @kitten_url = "http://lorempixel.com/#{requested_size}/#{requested_size}/cats"
+end
+
+def secrets
+  magic_word = "cats"
+  if params[:magic_word] == magic_word
+  else
+    flash[:notice] = "You do not have the correct credentials to enter this page"
+    redirect_to "/welcome"
+  end
+
 end
 
 end
